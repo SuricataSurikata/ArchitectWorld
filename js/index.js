d0be5fb15b0841d7535d722bac5a10d0c00e2505
Piotr Szymanski <piotr.szymanski@besidethepark.com>
2015-06-18 09:33:34 +0200

    style left sidebar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
-    $("#info").click(function(){
-        $("#info").hide();
+
+    $('#thumbnail').attr('src', 'assets/lisasmall.png');
+
+    //var elements = [{img: '', }, [], []];
+    //
+    //$('#elements div').each(function() {
+    //
+    //});
+
+
+
+    $('#elements div').click(function(){
+        $('.overlay').fadeIn(200);
+        $('#content p').html($(this).html());
+        $('#content img').attr('src', 'assets/lisasmall.png');
+        $('.close-window').hide();
     });
+
+    $('#content .close, #overlay').click(function(){
+       $('.overlay').fadeOut(200, function() {
+           $('.close-window').show();
+       });
+    });
+
+
 });
